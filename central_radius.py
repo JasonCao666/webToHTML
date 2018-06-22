72cbbb4ef8d951d7727eecc5a02f389067b18fbf
Jiahao Cao <s1722248@sms.ed.ac.uk>
2018-06-22 17:50:22 +0100

    [DEV] draw circle and sort distance

--- central_radius.py
+++ central_radius.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import csv
 from osgeo import ogr, osr
 
-print radians(3)
 #calculate the central point (specified in decimal degrees)
 def center_geolocation(geolocations):
     
@@ -17,65 +16,72 @@ def center_geolocation(geolocations):
     z = 0
     lenth = len(geolocations)
     for row in geolocations:
-        lon = radians(float(row.split(',')[0]))
-        lat = radians(float(row.split(',')[1]))
+        for word_row in row:
+            lon = math.radians(float(word_row.split(',')[0]))
+            lat = math.radians(float(word_row.split(',')[1]))
         
-        x += cos(lat) * cos(lon)
-        y += cos(lat) * sin(lon)
-        z += sin(lat)
+            x += math.cos(lat) * math.cos(lon)
+            y += math.cos(lat) * math.sin(lon)
+            z += math.sin(lat)
     
     x = float(x / lenth)
     y = float(y / lenth)
     z = float(z / lenth)
-    return [degrees(atan2(y, x)), degrees(atan2(z, sqrt(x * x + y * y)))]
+    return [math.degrees(math.atan2(y, x)), math.degrees(math.atan2(z, math.sqrt(x * x + y * y)))]
 
-def hav(theta):
-    s = sin(theta / 2)
+'''def hav(theta):
+    s = math.sin(theta / 2)
     return s * s
 
 #calculate the distance between two points on the earth (specified in decimal degrees)
 def haversine(lon1, lat1, lon2, lat2):
-    lon1=radians(lon1)
-    lat1=radians(lat1)
-    lon2=radians(lon2)
-    lat2=radians(lat2)
-    r = 6371 # earth r (km)
-    dlng = fabs(lon1 - lon2)
-    dlat = fabs(lat1 - lat2)
-    h = hav(dlat) + cos(lat1) * cos(lat2) * hav(dlng)
-    distance = 2 * r * asin(sqrt(h))*1000
-    
-    return distance
+    lon1=math.radians(lon1)
+    lat1=math.radians(lat1)
+    lon2=math.radians(lon2)
+    lat2=math.radians(lat2)
+    r = 6378.1 # earth r (km)
+    dlng = math.fabs(lon1 - lon2)
+    dlat = math.fabs(lat1 - lat2)
+    h = hav(dlat) + math.cos(lat1) * math.cos(lat2) * hav(dlng)
+    distance = 2 * r * math.asin(math.sqrt(h))*1000
+    return distance'''
 
 #The Haversine Functions to create a circle
-def createCircleAroundWithRadius(lat, lon, radiusMiles):
+def createCircleAroundWithRadius(latitude, longitude, radiusMiles):
     ring = ogr.Geometry(ogr.wkbLinearRing)
     latArray = []
     lonArray = []
 
     for brng in range(0,360):
-        lat2, lon2 = getLocation(lat,lon,brng,radiusMiles)
-        latArray.append(lat2)
-        lonArray.append(lon2)
+        latitude2, longitude2 = getLocation(latitude,longitude,brng,radiusMiles)
+        latArray.append(latitude2)
+        lonArray.append(longitude2)
     return lonArray,latArray
 
-def getLocation(lat1, lon1, brng, distanceMiles):
-    lat1 = lat1 * math.pi/ 180.0
-    lon1 = lon1 * math.pi / 180.0
+def getLocation(latitude1, longitude1, brng, distanceMiles):
+    latitude1 = latitude1 * math.pi/ 180.0
+    longitude1 = longitude1 * math.pi / 180.0
     #earth radius
-    R = 6378.1
+    R = 3959
     #R = ~ 3959 MilesR = 3959
             
     distanceMiles = distanceMiles/R
     brng = (brng / 90)* math.pi / 2
-    lat2 = math.asin(math.sin(lat1) * math.cos(distanceMiles)+ math.cos(lat1) * math.sin(distanceMiles) * math.cos(brng))
-    lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(distanceMiles)* math.cos(lat1),math.cos(distanceMiles)-math.sin(lat1)*math.sin(lat2))
-    lon2 = 180.0 * lon2/ math.pi
-    lat2 = 180.0 * lat2/ math.pi
-    return lat2, lon2
+    latitude2 = math.asin(math.sin(latitude1) * math.cos(distanceMiles)+ math.cos(latitude1) * math.sin(distanceMiles) * math.cos(brng))
+    longitude2 = longitude1 + math.atan2(math.sin(brng)*math.sin(distanceMiles)* math.cos(latitude1),math.cos(distanceMiles)-math.sin(latitude1)*math.sin(latitude2))
+    longitude2 = 180.0 * longitude2/ math.pi
+    latitude2 = 180.0 * latitude2/ math.pi
+    return latitude2, longitude2
+
+#get two points' distance in basemap
+def getDistanceBetweenTwoPoints(lo1,la1,lo2,la2):
+    distance_x=lo2-lo1
+    distance_y=la2-la1
+    return math.sqrt((distance_x**2)+(distance_y**2))
 
 
 word_coor_list=[]
+
 with open('/Users/mac/Documents/Dissertation/testDocument/haggies.csv','rb') as csv_word_coor:
     reader_word_coors = csv.DictReader(csv_word_coor,delimiter=',')
     for word_coors in reader_word_coors:
@@ -91,7 +97,7 @@ map = Basemap(llcrnrlon=-11.1,
               llcrnrlat=50.0,
               urcrnrlon=2.64,
               urcrnrlat=58.79,
-              resolution='h', projection='lcc', width=8E6, height=8E6, lat_0 =-2.16 ,lon_0=53.87)
+              resolution='h', projection='merc', lat_0 =-2.16 ,lon_0=53.87)
 
 map.drawlsmask(land_color='coral',ocean_color='aqua',lakes=True)
 map.drawcoastlines()
@@ -101,19 +107,31 @@ city_lons=[]
 city_lats=[]
 #city_names=[]
 coordinations=[]
+
 for word_coor_row in word_coor_list:
-    coordinations = word_coor_row['coordinations'].split()
-    central_point=center_geolocation(coordinations)
+    coordinations.append(word_coor_row['coordinations'].split())
+
+print coordinations
+central_point=center_geolocation(coordinations)
 print central_point
 
+central_point_lon,central_point_lat=map(central_point[1],central_point[0])
+distances=[]
 for coor_row in coordinations:
-    city_lons.append(float(coor_row.split(',')[1]))
-    city_lats.append(float(coor_row.split(',')[0]))
+    for word_row in coor_row:
+        city_lons.append(float(word_row.split(',')[1]))
+        city_lats.append(float(word_row.split(',')[0]))
+        map_p1,map_p2=map(float(word_row.split(',')[1]),float(word_row.split(',')[0]))
+        distances.append(getDistanceBetweenTwoPoints(central_point_lon,central_point_lat,map_p1,map_p2))
 
+print sorted(distances,reverse = True)
 x,y = map(city_lons, city_lats)
 central_x,central_y = map(central_point[1],central_point[0])
 map.plot(x, y, 'bo', markersize=5)
 map.plot(central_x, central_y, '-rx', markersize=15)
+
+
+
 
 '''r=2
 a,b=map(-3.19474665870404,55.60749302074808)
@@ -121,22 +139,22 @@ x2,y2 = map(-3.19474665870404,55.60749302074808+r)
 circle1 = plt.Circle((a, b), y2-b, color='black',fill=False)
 ax.add_patch(circle1)'''
 
-r=200000
 
-a,b=map(-3.19474665870404,55.60749302074808)
+c,d=map(-4.22398,57.47908)
+r_distance=getDistanceBetweenTwoPoints(central_point_lon,central_point_lat,c,d)
 theta=np.arange(0,2*np.pi, 0.01)
-cx=a+r*np.cos(theta)
-cy=b+r*np.sin(theta)
+cx=(central_point_lon+r_distance*np.cos(theta))
+cy=(central_point_lat+r_distance*np.sin(theta))
 map.plot(cx,cy, color='blue', linewidth=2.0)
 
 #test_p1,test_p2=map(-4.22398,57.47908)
-print haversine(-3.19474665870404,55.60749302074808,-4.22398,57.47908)
-X,Y = createCircleAroundWithRadius(55.60749302074808,-3.19474665870404,
-                                   200000)
+#print haversine(-3.19474665870404,55.60749302074808,-4.22398,57.47908)
+#X,Y = createCircleAroundWithRadius(55.60749302074808,-3.19474665870404,r/1.6/1000)
+#X,Y = map(X,Y)
+#map.plot(X,Y,marker=None,color='red',linewidth=4)
 
-X,Y = map(X,Y)
-mp.plot(X,Y,marker=None,color=c,linewidth=2)
-
+print getDistanceBetweenTwoPoints(central_point_lon,central_point_lat,c,d)
+print map.ymax - map.ymin
 plt.show()
 
 
